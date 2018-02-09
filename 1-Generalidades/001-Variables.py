e5774f5b3d1023dcec61bc807771b81b023fd9da
Víctor Darío Sosa Jáuregui <vidasoja@gmail.com>
2018-02-09 09:37:07 -0500

    Add files via upload

---
+++ 1-Generalidades/001-Variables.py
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------------------------
+#                         Universidad Global del Cusco
+#           Ingeniería en Tecnologías de Información y Comunicación
+#
+# Curso:       Algoritmos y Estructuras de Datos
+# Tema:        Variables
+# Contenido:   Declaración correcta e incorrecta devariables
+#
+# Autor:       Ing. Víctor Darío Sosa Jáuregui
+#
+# Copyright:   (c) VDSJ 2018
+# Licencia:    Creative Commons
+#-------------------------------------------------------------------------------
+
+mi_variable = 12    #Correcto
+MiVariable = 12     #Incorrecto
+mivariable = 12     #Incorrecto
+mi_variable=12      #Incorrecto
+mi_variable =   12  #Incorrecto
+
+"""
+    PEP 8: variables
+    Utilizar nombres descriptivos y en minúsculas. Para nombres
+    compuestos, separar las palabras por guiones bajos. Antes y
+    después del signo =, debe haber uno (y solo un) espacio en
+    blanco
+"""
