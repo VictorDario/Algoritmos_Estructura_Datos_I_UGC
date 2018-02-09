fb17c2c846c8dd347ddb565d2060a8cf8e202802
Víctor Darío Sosa Jáuregui <vidasoja@gmail.com>
2018-02-09 09:44:33 -0500

    Add files via upload

---
+++ 1-Generalidades/002-Constantes.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+#                         Universidad Global del Cusco
+#           Ingeniería en Tecnologías de Información y Comunicación
+#
+# Curso:       Algoritmos y Estructuras de Datos
+# Tema:        Constantes
+# Contenido:   Declaracion de constantes
+#              Existe un tipo de “variable”, denominada constante, la cual se
+#              utiliza para definir valores fijos, que no requieran ser
+#              modificados.
+#
+# Autor:       Ing. Víctor Darío Sosa Jáuregui
+#
+# Copyright:   (c) VDSJ 2018
+# Licencia:    Creative Commons
+#-------------------------------------------------------------------------------
+
+MI_CONSTANTE = 12
+print(MI_CONSTANTE)
+
+"""
+    PEP 8: constantes
+    Utilizar nombres descriptivos y en mayúsculas separando
+    palabras por guiones bajos.
+"""
